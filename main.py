e94b18cac0d8f25369e7cfbe77381027fa2beb72
Omar Shariff <78196064+kabeershariff@users.noreply.github.com>
2023-03-01 21:52:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
+
+class MainScreen(Screen):
+    search_box = ObjectProperty()
+	
+    def search(self):
+        print("Hehe")
 
 class ChatApp(MDApp):
     def build(self):
         self.theme_cls.theme_style="Dark"
         self.theme_cls.primary_palette="Orange"
+        return MainScreen()
 
 ChatApp().run()
